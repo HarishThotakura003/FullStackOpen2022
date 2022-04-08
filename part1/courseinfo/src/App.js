395b9c068920faef37dd7e519be32352a9f20236
HarishThotakura003 <thotakuraharish001@gmail.com>
2022-04-08 04:27:39 +0100

    Completed 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
 
+const Header =(props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  const data = props.data
+  return (
+    <div>
+      <p>{data[0]} {data[1]}</p>
+      <p>{data[2]} {data[3]}</p>
+      <p>{data[4]} {data[5]}</p>
+    </div>
+  )
+}
+
+const Total =(props) => {
+    const data = props.data
+    return (
+      <p>Number of exercises {data[0]+data[1]+data[2]}</p>
+    )
+}
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,11 +34,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content data={[part1,exercises1,part2,exercises2,part3,exercises3]} />
+      <Total data ={[exercises1,exercises2,exercises3]} />
     </div>
   )
 }
